e0a11005aaf36c7790627578d10fcb0e0a35a1fb
Your Name <you@example.com>
2021-06-14 15:36:04 +0700

    f

--- src/main/java/com/example/case6/model/Booking.java
+++ src/main/java/com/example/case6/model/Booking.java
@@ -1,21 +1,11 @@
 package com.example.case6.model;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Value;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.lang.Nullable;
-
 import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
-import java.io.Serializable;
 import java.sql.Date;
-import java.time.LocalDate;
+
 
 
 @Data
