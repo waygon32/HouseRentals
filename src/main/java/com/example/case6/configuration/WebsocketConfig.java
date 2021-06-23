8b56fcfccf0672794f2dc5909d205a7243a1a53d
waygon32 <truonthien2310@gmail.com>
2021-06-23 09:20:48 +0700

    d

--- src/main/java/com/example/case6/configuration/WebsocketConfig.java
+++ src/main/java/com/example/case6/configuration/WebsocketConfig.java
@@ -1,14 +1,26 @@
 package com.example.case6.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+//    @Autowired
+//    private WebSocketMessageBrokerStats webSocketMessageBrokerStats;
+//
+//    @PostConstruct
+//    public void init() {
+//        webSocketMessageBrokerStats.setLoggingPeriod(10 * 1000); // desired time in millis
+//    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry
